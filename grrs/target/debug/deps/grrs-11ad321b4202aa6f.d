9cf2975bac7ca48d429a56ab7417025423a1a599
Astrid Pulse <astridcpulse@gmail.com>
2024-08-26 23:26:38 -0500

    initial commit

---
+++ grrs/target/debug/deps/grrs-11ad321b4202aa6f.d
@@ -0,0 +1,5 @@
+/Users/asty/programming/rust-projects/cli-tool/grrs/target/debug/deps/libgrrs-11ad321b4202aa6f.rmeta: src/main.rs
+
+/Users/asty/programming/rust-projects/cli-tool/grrs/target/debug/deps/grrs-11ad321b4202aa6f.d: src/main.rs
+
+src/main.rs:
---
+++ grrs/target/debug/deps/grrs-e95a28e3455e3759.d
@@ -0,0 +1,5 @@
+/Users/asty/programming/rust-projects/cli-tool/grrs/target/debug/deps/libgrrs-e95a28e3455e3759.rmeta: src/main.rs
+
+/Users/asty/programming/rust-projects/cli-tool/grrs/target/debug/deps/grrs-e95a28e3455e3759.d: src/main.rs
+
+src/main.rs:
